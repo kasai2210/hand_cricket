4f943061dfbc58b9cddf7a8ae92883401d407b42
Bhoopen <62685221+kasai2210@users.noreply.github.com>
2020-08-16 20:25:10 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,75 @@
+# Import libraries
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import keras 
+import h5py
+import cv2
+import os
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+
+IMG_SAVE_PATH = 'image_data'
+
+CLASS_MAP = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5
+}
+
+NUM_CLASSES = len(CLASS_MAP)
+
+def mapper(val):
+    return CLASS_MAP[val]
+
+model=Sequential()
+model.add(Conv2D(32,(3,3),input_shape=(64,64,3),
+    padding='same',activation='relu',
+    kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Conv2D(64,(5,5),activation='relu',padding='same',kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(128,(3,3),activation='relu',padding='same',kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+model.add(Dense(512,activation='relu',kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.5))
+model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+dataset = []
+for directory in os.listdir(IMG_SAVE_PATH):
+    path = os.path.join(IMG_SAVE_PATH, directory)
+    if not os.path.isdir(path):
+        continue
+    for item in os.listdir(path):
+        # to make sure no hidden files get in our way
+        if item.startswith("."):
+            continue
+        img = cv2.imread(os.path.join(path, item))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (64, 64))
+        dataset.append([img, directory])
+
+data, labels = zip(*dataset)
+labels = list(map(mapper, labels))
+
+labels = np_utils.to_categorical(labels)
+
+# train the model
+model.fit(np.array(data), np.array(labels), validation_split=0.2, epochs=5, batch_size=64)
+
+# save the model for later use
+model.save("model_1.h5")
